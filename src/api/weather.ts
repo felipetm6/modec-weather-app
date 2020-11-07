2181a628724a3e4e22b5e472d21c9a756cdf7496
Felipe Teixeira Moraes <felipetm6@hotmail.com>
2020-11-07 18:51:20 -0300

    feat: create initial setup for OpenWeather API's consumption

---
+++ src/api/weather.ts
@@ -0,0 +1,26 @@
+import { OpenWeatherData } from 'models/weather';
+
+const createOpenWeatherRequest = async (
+  query: string,
+): Promise<BulkOpenWeatherResponse> =>
+  await fetch(
+    `http://api.openweathermap.org/data/2.5/${query}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`,
+  ).then((response) => response.json());
+
+type BulkOpenWeatherResponse = {
+  cod: string;
+  count: number;
+  message: string;
+  list: OpenWeatherData[];
+};
+
+export const getForecastByCoordinates = async (
+  latitude: number,
+  longitude: number,
+): Promise<BulkOpenWeatherResponse> => {
+  const response = await createOpenWeatherRequest(
+    `find?lat=${latitude}&lon=${longitude}&cnt=15`,
+  );
+
+  return response;
+};
